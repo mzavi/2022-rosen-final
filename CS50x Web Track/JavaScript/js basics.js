6b874798a7ec6ac230f7e4b0f980442c87d3739f
Michael Zavi <99049169+vareq1@users.noreply.github.com>
2022-11-28 12:20:32 -0500

    JavaScript Lecture

---
+++ CS50x Web Track/JavaScript/js basics.js
@@ -0,0 +1,41 @@
+
+let counter = 0
+counter = counter + 1
+counter += 1
+counter ++
+
+if (x > y)
+{
+    print(counter)
+}
+else
+{
+    counter += 1
+};
+
+
+while (true)
+{
+
+};
+
+"using a FOR LOOP to iteratoe over things:"
+
+for (let i = 0; i < 50; i++)
+{
+    counter++
+}
+
+"CREATING A FUNCTION"
+"function name(input type not needed to specify)"
+
+function cough(n)
+{
+    let i = 0
+    for (let x = 0; x <10; x++)
+    {
+        i += 5
+    }
+
+    return i
+}
